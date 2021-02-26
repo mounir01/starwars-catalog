4506e7d579d899146889463c0828eeb523cb7234
mounir01 <mounir.mechqrane@gmail.com>
2021-02-26 13:12:10 -0500

    Add routing to the project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule} from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-
+import { AboutComponent } from './about/about.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { StarshipsComponent } from './starships/starships.component';
+import { StarshipCardComponent } from './starship-card/starship-card.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }];
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'categories', component: CategoriesComponent },
+  { path: 'categories/starships', component: StarshipsComponent },
+  { path: 'categories/starships/:id', component: StarshipCardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
